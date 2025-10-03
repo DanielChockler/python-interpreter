3ddfc21ffe298f87c265e8c012e95237c9003a89
Daniel Chockler <daniel.chockler@gmail.com>
2025-10-03 13:03:27 +0100

    Fixed bugs, changed how S Expressions were parsed, implemented pretty printing method, improved parser output

--- parser.py
+++ parser.py
@@ -1,5 +1,22 @@
 from lexer import Lexer
 
+class Token:
+	def __init__(self, type, value):
+		self.type = type
+		self.value = value
+
+	def __repr__(self):
+		return f'{self.type} {self.value}'
+
+class SExpr:
+	def __init__(self, contents):
+		self.contents = contents
+
+	def __repr__(self):
+		res = ','.join(map(str, self.contents))
+		return f'SExpr({res})'
+
+
 class Parser:
 
 	def __init__(self, inp):
@@ -8,64 +25,58 @@ class Parser:
 		self.currentToken = self.inp[self.pos] if self.pos < len(self.inp) else None
 
 	def getNextToken(self):
+		# Advance token stream
 		self.pos += 1
 
-		if self.pos < len(self.inp) and self.inp[self.pos] is not None:
+		if self.pos < len(self.inp):
 			self.currentToken = self.inp[self.pos]
 
 		else:
 			self.currentToken = None
 
 	def parseExpr(self):
+		# If token is an opening bracket, start parsing the S expression
 		if self.currentToken[1] == '(':
 			return self.parseSExpr()
 
+		# Finding a ')' here is an error
 		elif self.currentToken[1] == ')':
 			raise Exception('Unexpected closing bracket')
 
 		else:
-			return self.currentToken
+			atom = Token(self.currentToken[0], self.currentToken[1])
+			self.getNextToken() # Consume current token
+			return atom
 
 	def parseSExpr(self):
-		self.getNextToken()
-		SExpr = '('
-		res = ['SEXPR']
-		subExpr = []
-		try:
-			while self.currentToken[1] != ')':
+		self.getNextToken() # Consume first '('
 
-				if self.currentToken[1] == '(':
-					subExpr.append(self.parseSExpr())
+		res = []
+		# Recursively call parseExpr() until finding ')'
+		while self.currentToken is not None and self.currentToken[1] != ')':
+			res.append(self.parseExpr())
 
-				SExpr += str(self.currentToken)
-
-				self.getNextToken()
-			SExpr += ')'
-			res.append(SExpr)
-			res.extend(subExpr)
-
-			return res
-
-		except TypeError:
+		# Ran out of tokens before finding a closing bracket, so it's an error 
+		if self.currentToken is None:
 			raise Exception('Expected closing bracket')
+
+		# Consume ')'
+		self.getNextToken()
+
+		return SExpr(res)
 
 	def parseProgram(self):
 		res = []
 
-		while self.pos < len(self.inp) and self.currentToken is not None:
-			if self.currentToken[0] == 'SYMBOL':
-				res.append(self.parseExpr())
-
-			else:
-				res.append(self.currentToken)
-
-			self.getNextToken()
+		while self.currentToken is not None:
+			res.append(self.parseExpr())
 
 		return res
 
-inp = '(5 + 5 + (10 + 2)) + he()'
-lexer = Lexer(inp)
-inp = lexer.lex()
+	def prettyPrinting(self):
+		ls = self.parseProgram() # Make sure there are no errors in input even when only pretty printing
+		res = ''
+		for i in self.inp:
+			res += str(i[1]) + ' '
 
-parser = Parser(inp)
-print(parser.parseProgram())
+		return res
